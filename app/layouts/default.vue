a6dde0fe4f1a6c59d25d1338cfb1dc2a2fde6ec9
J-Cupani <cupani.dev@gmail.com>
2025-02-27 12:55:08 +0100

    add webp format

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,7 @@
       <div class="container mx-auto p-4">
         <h1 class="text-3xl font-bold text-f-light">Bienvenue sur Sakai Vue</h1>
         <p class="text-f-light">Une plateforme optimisée pour le SEO avec Nuxt et Tailwind CSS</p>
-        <NuxtImg src="space-inv.png" alt="Illustration de Sakai Vue" class="rounded-lg mt-4" width="100" height="100" />
+        <NuxtImg src="space-inv.png" alt="Illustration de Sakai Vue" class="rounded-lg mt-4" width="100" height="100" format="webp"/>
       </div>
     </header>
 
@@ -16,12 +16,12 @@
         <article class="bg-f-light p-6 rounded-lg shadow-md mx-2">
           <h3 class="text-xl font-bold text-f-dark mb-2">Interface intuitive</h3>
           <p class="text-f-dark">Profitez d'une interface simple et ergonomique, facile à utiliser.</p>
-          <NuxtImg src="profil.jpg" alt="Aperçu de l'interface" class="mt-4 rounded" width="222" height="222" />
+          <NuxtImg src="profil.jpg" alt="Aperçu de l'interface" class="mt-4 rounded" width="222" height="222" format="webp"/>
         </article>
         <article class="bg-f-light p-6 rounded-lg shadow-md">
           <h3 class="text-xl font-bold text-f-dark mb-2">Intégration facile</h3>
           <p class="text-f-dark">Connectez Sakai Vue à vos outils préférés en quelques clics.</p>
-          <NuxtImg src="profil.jpg" alt="Aperçu de l'interface" class="mt-4 rounded" width="222" height="222" />
+          <NuxtImg src="profil.jpg" alt="Aperçu de l'interface" class="mt-4 rounded" width="222" height="222" format="webp"/>
         </article>
       </div>
     </section>
@@ -48,12 +48,6 @@
 
 <script setup>
 onMounted(() => {
-  // Charger dynamiquement les styles uniquement pour ce layout
   import('@/assets/css/tailwind.css');
-});
+})
 </script>
-
-
-<style lang="scss" scoped>
-
-</style>
